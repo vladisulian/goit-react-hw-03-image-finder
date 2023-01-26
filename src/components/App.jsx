56c2947591b8b29d3425401b15154fbf4221fdff
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-01-26 13:54:52 +0200

    tested fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { fetchImages } from './fetch';
+import { Component } from 'react';
+export class App extends Component {
+  state = {
+    inputValue: '',
+  };
+  handleChange = e => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          name="inputValue"
+          value={this.state.inputValue}
+          onChange={this.handleChange}
+        />
+        <button
+          type="submit"
+          onClick={() => {
+            fetchImages(this.state.inputValue);
+          }}
+        >
+          Send request
+        </button>
+      </div>
+    );
+  }
+}
--- src/components/fetch.js
+++ src/components/fetch.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-async function fetchImages(value, pageNumber) {
+export async function fetchImages(value, pageNumber) {
   const responce = await axios.get(
-    `https://pixabay.com/api/?q=${value}&page=1&key=31911886-b70f9395e6a878339225ca253&image_type=photo&orientation=horizontal&per_page=12`
+    `https://pixabay.com/api/?q=${value}&page=1&key=31911886-b70f9395e6a878339225ca253&image_type=photo&orientation=horizontal&per_page=12&page=${pageNumber}`
   );
+  console.log(responce.data.hits);
+  return responce.data;
 }
