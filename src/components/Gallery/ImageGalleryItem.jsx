e2dfb527b31f9804121606f1c2505166e4fc69b9
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-01-27 14:03:54 +0200

    Update ImageGalleryItem.jsx

--- src/components/Gallery/ImageGalleryItem.jsx
+++ src/components/Gallery/ImageGalleryItem.jsx
@@ -2,14 +2,12 @@ export const ImageGalleryItem = ({ images }) => {
   return images.map(image => {
     return (
       <li className="ImageGalleryItem" key={image.id}>
-        <a href={image.largeImageURL} loading="lazy" className="lightbox-a">
-          <img
-            src={image.webformatURL}
-            alt={image.tags}
-            className="ImageGalleryItem-image"
-            title="title"
-          />
-        </a>
+        <img
+          src={image.webformatURL}
+          alt={image.tags}
+          className="ImageGalleryItem-image"
+          title="title"
+        />
       </li>
     );
   });
