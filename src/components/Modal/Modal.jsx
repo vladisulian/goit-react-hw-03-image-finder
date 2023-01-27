ffd1db1d9edacc4dd4ee2a1a38cbb1303767fded
Vladislav Ulianeev <vladis.ulian@gmail.com>
2023-01-27 14:56:54 +0200

    add portal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,15 @@
 import { Component } from 'react';
+import { createPortal } from 'react-dom';
+
+const modalRoot = document.querySelector('#modal-root');
+
 export class Modal extends Component {
   render() {
-    return (
+    return createPortal(
       <div className="Overlay">
         <div className="Modal">{this.props.children}</div>
-      </div>
+      </div>,
+      modalRoot
     );
   }
 }
